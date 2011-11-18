0837899def6b24e8f4b9909d9f97a6a15fe71c53
Juampa <juampa@informa.es>
2011-11-18 23:21:44 +0100

    Simplificamos el mensaje enviado y la gráfica

--- server.js
+++ server.js
@@ -3,48 +3,18 @@ var app = require('http').createServer(handler)
   , fs = require('fs')
   , sys = require('sys');
 
-var iostat = require('child_process').spawn("iostat", ["-w 5"]);
+var iostat = require('child_process').spawn("tail", ["-f", "-n 1", "/tmp/pruebas"]);
 
 app.listen(8000);
 
-var datos = new Array(10) ;
-var puntero = 0 ;
-var size = 10 ;
-
-
 function format (data) {
-  var output_data = data.toString();
-  sys.log(output_data);
-  header = 'disk0       cpu     load average'
-  if (output_data.match(header)) {
-    sys.log("ignore header")
-  }else{
-    // disk0 cpu load 
-    // average kbt tps kbs us sy id 1m 5m 15m
-    var output_array = output_data.replace(/^\s+|\s+$/g,"").split(/\s+/);
-    for (var i=0; i < output_array.length; i++) {
-      output_array[i] = parseFloat( output_array[i]);
-    };
-    output_hash = {
+  var output_data = data.toString('utf8');
+  //sys.log(output_data);
+  output_hash = {
       date:new Date(),
-      disk:{
-        kbt:output_array[0],
-        tps:output_array[1],
-        mbs:output_array[2]
-      },
-      cpu:{
-        us:output_array[3],
-        sy:output_array[4],
-        id:output_array[5]
-      },
-      load_average:{
-        m1:output_array[6],
-        m5:output_array[7],
-        m15:output_array[8]
-      }
-    }
-    return JSON.stringify(output_hash);
+      value:output_data
   }
+  return JSON.stringify(output_hash);
 }
 
 function handler (req, res) {
@@ -62,8 +32,8 @@ function handler (req, res) {
 io.sockets.on('connection', function (socket) {
   console.log('nueva conexion') ;
   iostat.stdout.on('data', function (data) {
-    sys.log(typeof(data));
-    sys.log('server stdout: ' + format(data));
+    sys.log(data.toString('utf8'));
+    //sys.log('server stdout: ' + format(data));
   	socket.emit('news',  format(data));
     //socket.on('my other event', function (data) {
     	//console.log(data);
